dd54bb53c9e0d80d33b7af4192ba956afb42d524
Zack Bartel <zack@bartel.com>
2019-10-12 20:30:44 -0700

    foo

--- go/guestbook/go.mod
+++ go/guestbook/go.mod
@@ -3,8 +3,7 @@ module github.com/zackb/code/go/guestbook
 go 1.13
 
 require (
-	github.com/blend/go-sdk v2.0.0+incompatible // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/wcharczuk/go-chart v2.0.1+incompatible
-	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	github.com/chenjiandongx/go-echarts v0.0.0-20190915064101-cbb3b43ade5d
+	github.com/go-echarts/go-echarts v0.0.0-20190915064101-cbb3b43ade5d // indirect
+	github.com/gobuffalo/packr v1.30.1 // indirect
 )
--- go/guestbook/guest.go
+++ go/guestbook/guest.go
@@ -5,13 +5,13 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strings"
 	"time"
 
-	"github.com/wcharczuk/go-chart"
+	"github.com/chenjiandongx/go-echarts/charts"
 )
 
 const (
@@ -23,18 +23,46 @@ type person struct {
 	date      time.Time
 	firstName string
 	lastName  string
+	note      string
 }
 
 type people []person
 
+type report struct {
+	people people
+	cp     people
+	noncp  people
+}
+
+type month struct {
+	idx        int
+	label      string
+	count      int
+	cpCount    int
+	nonCpCount int
+}
+
+type dataGroup map[int]int
+
 func main() {
-	ppl := readPeople()
-	fmt.Println(ppl)
+	r := readReport()
+	drawGraph(r)
+}
+
+func readReport() report {
+	var report report
+	for _, p := range readPeople() {
+		report.addPerson(p)
+	}
+	return report
 }
 
 func readPeople() people {
 	var res people
-	for _, line := range readLines() {
+	for i, line := range readLines() {
+		if i == 0 && header {
+			continue
+		}
 		res = append(res, newPerson(line))
 	}
 
@@ -62,33 +90,6 @@ func readLines() []string {
 	return lines
 }
 
-func drawGraph() {
-
-	graph := chart.BarChart{
-		Title: "BWZ Guestbook",
-		Background: chart.Style{
-			Padding: chart.Box{
-				Top: 40,
-			},
-		},
-		Height:   512,
-		BarWidth: 60,
-		Bars: []chart.Value{
-			{Value: 5.25, Label: "Blue"},
-			{Value: 4.88, Label: "Green"},
-			{Value: 4.74, Label: "Gray"},
-			{Value: 3.22, Label: "Orange"},
-			{Value: 3, Label: "Test"},
-			{Value: 2.27, Label: "??"},
-			{Value: 1, Label: "!!"},
-		},
-	}
-
-	f, _ := os.Create("output.png")
-	defer f.Close()
-	graph.Render(chart.PNG, f)
-}
-
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -97,8 +98,111 @@ func check(e error) {
 
 func newPerson(line string) person {
 	parts := strings.Split(line, ",")
+	d, err := time.Parse("1/2/2006", parts[0])
+	check(err)
 	return person{
-		date:      time.Now(),
+		date:      d,
 		firstName: parts[1],
+		lastName:  parts[2],
+		note:      parts[9],
 	}
+}
+
+func (r *report) addPerson(p person) {
+	r.people = append(r.people, p)
+	if p.note == "CP" {
+		r.cp = append(r.cp, p)
+	} else {
+		r.noncp = append(r.noncp, p)
+	}
+}
+
+func (p *person) dateGroupKey() int {
+	return 10*int(p.date.Year()) + int(p.date.Month())
+}
+
+func (p *person) monthLabel() string {
+	return p.date.Format("Jan")
+}
+
+func (p *person) monthAndYearLabel() string {
+	return p.date.Format("Jan/2006")
+}
+
+func (p *people) series() map[string]int {
+	s := map[string]int{}
+
+	for _, per := range *p {
+		s[per.monthLabel()]++
+	}
+	return s
+}
+
+// counts by year/month
+func (p *people) grouped() dataGroup {
+	s := dataGroup{}
+
+	for _, per := range *p {
+		s[per.dateGroupKey()]++
+	}
+	return s
+}
+
+func (d dataGroup) sort() (index []int) {
+	for k := range d {
+		index = append(index, k)
+	}
+	sort.Ints(index)
+	return
+}
+
+func (r *report) months() []month {
+	months := make(map[int]month, len(r.people))
+
+	for _, p := range r.people {
+		k := p.dateGroupKey()
+		m := months[k]
+		m.idx = k
+		m.label = p.monthLabel()
+		m.count++
+		if p.note == "CP" {
+			m.cpCount++
+		} else {
+			m.nonCpCount++
+		}
+		months[k] = m
+	}
+
+	var vals []month
+	for _, m := range months {
+		vals = append(vals, m)
+	}
+	sort.Slice(vals, func(i, j int) bool {
+		return vals[i].idx < vals[j].idx
+	})
+
+	return vals
+}
+
+func drawGraph(r report) {
+	var labels []string
+	var values []int
+	var cp []int
+	var nocp []int
+	for _, m := range r.months() {
+		labels = append(labels, m.label)
+		values = append(values, m.count)
+		cp = append(cp, m.cpCount)
+		nocp = append(nocp, m.nonCpCount)
+	}
+
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "BWZ Guestbook"})
+	bar.AddXAxis(labels).
+		AddYAxis("People", values)
+		//AddYAxis("CP", cp).
+		//AddYAxis("Not CP", nocp)
+	f, err := os.Create("output.html")
+	check(err)
+	bar.Render(f)
 }
