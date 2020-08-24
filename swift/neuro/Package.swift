c058a0a3db0af529716bf458d05c14accba2fd1b
Zack Bartel <zack@bartel.com>
2020-08-24 15:54:50 -0700

    Http.swift -> Swifter

--- swift/neuro/Package.swift
+++ swift/neuro/Package.swift
@@ -4,13 +4,17 @@ import PackageDescription
 
 let package = Package(
     name: "neuro",
+    products: [
+        .executable(name: "neuro", targets: ["neuro"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/BiAtoms/Http.swift.git", from: "2.2.1")
+        // .package(url: "https://github.com/BiAtoms/Http.swift.git", from: "2.2.1")
+        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.4.7"))
     ],
     targets: [
         .target(
             name: "neuro",
-            dependencies: ["HttpSwift"]),
+            dependencies: ["Swifter"]),
         .testTarget(
             name: "neuroTests",
             dependencies: ["neuro"]),
