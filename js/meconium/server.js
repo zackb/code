1997e91518be3430d8c6c83fb6082a88b57246fc
Zack Bartel <zack@bartel.com>
2020-11-02 13:56:51 -0800

    relearn js

--- js/meconium/server.js
+++ js/meconium/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static("public"));
 app.use(express.static("build"));
+app.use(express.static("public"));
 
 app.get('/message', (req, res) => {
   res.json({message: "Hello, World!"});
