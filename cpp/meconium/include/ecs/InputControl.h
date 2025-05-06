967d021e719a949692d75d3de98fa0ed278f65b5
Zack Bartel <zack@bartel.com>
2025-05-05 19:33:32 -0700

    refactor input, jump

--- cpp/meconium/include/ecs/InputControl.h
+++ cpp/meconium/include/ecs/InputControl.h
@@ -2,9 +2,59 @@
 
 #include "Component.h"
 
+struct InputAction {
+    SDL_Scancode key;
+    bool isPressed = false;
+    bool wasPressed = false;
+
+    InputAction(SDL_Scancode scancode) : key(scancode) {}
+
+    void update(const Uint8 *keyboardState) {
+        wasPressed = isPressed;
+        isPressed = keyboardState[key];
+    }
+
+    bool justPressed() const {
+        return isPressed && !wasPressed;
+    }
+
+    bool justReleased() const {
+        return !isPressed && wasPressed;
+    }
+};
+
+
+enum class InputKey {
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    JUMP
+};
+
 class InputControl : public Component
 {
 public:
-    bool up, down, left, right;
-    InputControl() : up(false), down(false), left(false), right(false) {}
+
+    std::unordered_map<InputKey, InputAction> actions;
+
+    InputControl() {
+        actions.emplace(InputKey::MOVE_LEFT, InputAction(SDL_SCANCODE_LEFT));
+        actions.emplace(InputKey::MOVE_RIGHT, InputAction(SDL_SCANCODE_RIGHT));
+        actions.emplace(InputKey::JUMP, InputAction(SDL_SCANCODE_UP));
+    }
+
+    void update(const Uint8 *keyboardState) {
+        for (auto& [name, action] : actions) {
+            action.update(keyboardState);
+        }
+    }
+
+    bool isDown(InputKey key) const {
+        auto it = actions.find(key);
+        return it != actions.end() && it->second.isPressed;
+    }
+
+    bool justPressed(InputKey key) const {
+        auto it = actions.find(key);
+        return it != actions.end() && it->second.justPressed();
+    }
 };
