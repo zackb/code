f5731ce00f893c042db46e513be4a2e88269ac5b
Zack Bartel <zack@bartel.com>
2025-05-09 12:05:01 -0700

    first cut at parallax background

--- cpp/meconium/include/systems/RenderSystem.h
+++ cpp/meconium/include/systems/RenderSystem.h
@@ -2,11 +2,18 @@
 
 #include "TileMap.h"
 
+struct ParallaxLayer {
+    SDL_Texture* texture;
+    float speed;
+};
+
 class RenderSystem {
 public:
     void render(const std::vector<std::shared_ptr<Entity>>& entities, TileMap& tileMap);
 
 private:
+    void renderParallaxBackground(Transform& camera);
+    void renderLayer(const ParallaxLayer& layer, const Transform& camera);
     void renderTileMap(TileMap& tileMap, Transform& camera);
     std::shared_ptr<Entity> findActiveCamera(const std::vector<std::shared_ptr<Entity>>& entities) const;
 };
