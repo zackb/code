4c9b690bbc89fe14aebe07b4a94148323d454dd9
Zack Bartel <zack@bartel.com>
2025-05-15 14:58:41 -0700

    bug fix

--- cpp/meconium/src/systems/EnemyAISystem.cpp
+++ cpp/meconium/src/systems/EnemyAISystem.cpp
@@ -1,5 +1,6 @@
 #include "systems/EnemyAISystem.h"
 
+#include "components/Sprite.h"
 #include "components/State.h"
 #include "components/Tag.h"
 #include "components/Transform.h"
@@ -15,33 +16,44 @@ void EnemyAISystem::update(const std::shared_ptr<Entities> &entities) const {
         auto position = entity->getComponent<Transform>();
         auto velocity = entity->getComponent<Velocity>();
         auto state = entity->getComponent<State>();
+        auto sprite = entity->getComponent<Sprite>();
 
-        if (!position || !velocity || !state) {
+        auto player = entities->findEntityWithComponent<PlayerTag>();
+        auto playerPos = player->getComponent<Transform>();
+
+        if (!position || !velocity || !state || !sprite) {
             std::cerr << "missing required components of enemy" << std::endl;
             continue;
         }
 
         velocity->vx = 0;
-
-        std::cout << entity->id << std::endl;
-        std::cout << position->x << " " << position->y << std::endl;
         // TODO: parameterize patrol bounds
         // "x": 1200,
         // "y": 1200,
         switch (state->currentAction) {
             case Action::IDLE:
+                // TODO: direction should be centralized (Facing component?)
+                // to support sprites facing different directions
+                if (playerPos->x > position->x) {
+                    state->facingRight = true;
+                    sprite->flipX = false;
+                } else {
+                    state->facingRight = false;
+                    sprite->flipX = true;
+                }
                 break;
             case Action::PATROLLING:
                 if (state->facingRight) {
                     velocity->vx = 2; // TODO: prefab?
                     if (position->x >= 1400) {
                         state->facingRight = false;
+                        sprite->flipX = true;
                     }
                 } else {
                     velocity->vx = -2;
                     if (position->x <= 800) {
                         state->facingRight = true;
-                        position->x = 800;
+                        sprite->flipX = false;
                     }
                 }
                 break;
