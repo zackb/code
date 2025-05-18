6f12434d9a1d6112c44b4327a9235793ff04fc2f
Zack Bartel <zack@bartel.com>
2025-05-18 16:21:22 -0700

    some cleanup

--- cpp/meconium/include/systems/LifecycleSystem.h
+++ cpp/meconium/include/systems/LifecycleSystem.h
@@ -7,20 +7,17 @@ class LifecycleSystem {
 public:
     void update(const std::shared_ptr<Entities>& entities, const int deltaTime) const {
 
-        std::vector<std::shared_ptr<Entity>> toRemove;
-
         for (auto& entity : *entities) {
             auto despawn = entity->getComponent<Despawn>();
             if (despawn) {
                 despawn->timer -= deltaTime;
                 if (despawn->timer <= 0) {
-                    toRemove.push_back(entity);
+                    // entity should despawn add to removal queue
+                    entities->queueRemove(entity);
                 }
             }
         }
 
-        for (const auto e : toRemove) {
-            entities->remove(e);
-        }
+        entities->flushQueue();
     }
 };
