198281d5fce7c424e0f8665a8d9bf52a99a46fed
Zack Bartel <zack@bartel.com>
2025-05-18 19:04:44 -0700

    prefab attack damage and range

--- cpp/meconium/include/components/Attack.h
+++ cpp/meconium/include/components/Attack.h
@@ -7,10 +7,15 @@
 struct Attack : Component {
     std::shared_ptr<SpriteSheetDefinition> sprite;
     int cooldownMs;
+    int damage;
     int attackRange;
 };
 
 struct Projectile : Component {
     int lifetimeMs;
+    int damage;
     int ageMs = 0;
+
+    Projectile(int lifetimeMs) : lifetimeMs(lifetimeMs) {}
+    Projectile(int lifetimeMs, int damage) : lifetimeMs(lifetimeMs), damage(damage) {}
 };
