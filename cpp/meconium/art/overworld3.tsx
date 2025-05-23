3a819ef8cb16b593e591a78b1e6c3d12ef415710
Zack Bartel <zack@bartel.com>
2025-05-22 20:16:32 -0700

    level3 work

---
+++ cpp/meconium/art/overworld3.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="overworld3" tilewidth="16" tileheight="16" tilecount="504" columns="28">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../assets/textures/overworld3.png" width="448" height="288"/>
+</tileset>
