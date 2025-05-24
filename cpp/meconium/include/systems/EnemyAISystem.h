e3583a843566c8ec992cfbc615b4d7ced7db5fc5
Zack Bartel <zack@bartel.com>
2025-05-23 21:26:09 -0700

    some cleanup

--- cpp/meconium/include/systems/EnemyAISystem.h
+++ cpp/meconium/include/systems/EnemyAISystem.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "components/Attack.h"
+#include "components/Sprite.h"
+#include "components/State.h"
 #include "components/Transform.h"
+#include "components/Velocity.h"
 #include "entity/Entity.h"
 
 class EnemyAISystem {
@@ -9,5 +12,8 @@ public:
     void update(const std::shared_ptr<Entities>& entities, const int dt) const;
 
 private:
+    void performBehavior(
+        EnemyAI& ai, Sprite& sprite, State& state, Transform& position, Velocity& velocity, Transform& playerPos) const;
+
     bool seesTarget(Transform& playerPos, Transform& enemyPos, Attack& attack, bool facingRight) const;
 };
