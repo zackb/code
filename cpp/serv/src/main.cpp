0525f734b7e7ca5e9fadb382e39431fb412bcc96
Zack Bartel <zack@bartel.com>
2025-06-08 18:50:26 -0700

    i swear im not doing this

--- cpp/serv/src/main.cpp
+++ cpp/serv/src/main.cpp
@@ -1,12 +1,34 @@
+#include <unordered_map>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
 #include "cpr/response.h"
 #include <cpr/cpr.h>
 #include <cstdlib>
+#include <httplib.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 int main(int argc, const char* argv[]) {
 
     cpr::Response resp = cpr::Get(cpr::Url{"https://ifconfig.co/json"});
     std::cout << resp.text << std::endl;
 
+    httplib::Client cli("https://ifconfig.co");
+    auto resp1 = cli.Get("/json");
+
+    std::cout << resp1->body << std::endl;
+
+    httplib::Server svr;
+
+    svr.Get("/api/v1/info", [](const httplib::Request& req, httplib::Response& res) {
+        std::unordered_map<std::string, std::string> map;
+        map["remote-addr"] = req.remote_addr;
+        map["user-agent"] = req.get_header_value("user-agent");
+        nlohmann::json j = map;
+        res.set_content(j.dump(), "application/json");
+        res.status = 200;
+    });
+
+    svr.listen("0.0.0.0", 8080);
     return EXIT_SUCCESS;
 }
