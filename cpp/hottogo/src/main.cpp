549bcbd59eb76d1ca10cd595dc7676cdbc2406e6
Zack Bartel <zack@bartel.com>
2025-06-09 13:50:06 -0700

    resp

--- cpp/hottogo/src/main.cpp
+++ cpp/hottogo/src/main.cpp
@@ -12,17 +12,30 @@ int main(int argc, const char* argv[]) {
 
     std::cout << resp1->body << std::endl;
 
-    httplib::Server svr;
+    httplib::Server srv;
 
-    svr.Get("/api/v1/info", [](const httplib::Request& req, httplib::Response& res) {
+    srv.Get("/api/v1/req", [](const httplib::Request& req, httplib::Response& res) {
         std::unordered_map<std::string, std::string> map;
         map["remote-addr"] = req.remote_addr;
         map["user-agent"] = req.get_header_value("user-agent");
+        for (auto& [k, v] : req.headers) {
+            map[k] = v;
+        }
+
+        for (auto& [k, v] : req.params) {
+            map[k] = v;
+        }
+
         nlohmann::json j = map;
         res.set_content(j.dump(), "application/json");
         res.status = 200;
     });
 
-    svr.listen("0.0.0.0", 8080);
+    srv.Get("/api/v1/resp", [](const httplib::Request& req, httplib::Response& res) {
+        res.status = req.get_param_value("status").empty() ? 200 : std::stoi(req.get_param_value("status"));
+    });
+
+    srv.listen("0.0.0.0", 8080);
+
     return EXIT_SUCCESS;
 }
