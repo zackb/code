f70357641439944df3c36aa5f68c6edf27ac227f
Zack Bartel <zack@bartel.com>
2025-06-08 19:02:44 -0700

    clean

--- cpp/hottogo/src/main.cpp
+++ cpp/hottogo/src/main.cpp
@@ -1,18 +1,12 @@
 #include <unordered_map>
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
-#include "cpr/response.h"
-#include <cpr/cpr.h>
 #include <cstdlib>
 #include <httplib.h>
 #include <iostream>
 #include <nlohmann/json.hpp>
 
 int main(int argc, const char* argv[]) {
-
-    cpr::Response resp = cpr::Get(cpr::Url{"https://ifconfig.co/json"});
-    std::cout << resp.text << std::endl;
-
     httplib::Client cli("https://ifconfig.co");
     auto resp1 = cli.Get("/json");
 
