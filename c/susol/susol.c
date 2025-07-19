d5bca25a9290865457ec3b7f9855d7dfd55d1cb0
Zack Bartel <zack@bartel.com>
2025-07-18 23:14:50 -0700

    fix up some old code

--- c/susol/susol.c
+++ c/susol/susol.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define SIZE 9
 #define EMPTY 0
 
-int usage();
-void read_board();
-int solve_board();
-void print_board();
-
 int board[SIZE][SIZE] = {{EMPTY}};
-
-int main(int argc, const char **argv) {
-    if (argc != 2) 
-        return usage();
-    read_board(argv[1]);
-    solve_board();
-    print_board();
-    return 0;
-}
 
 int solve_board() {
     // find the x and y to try
-   int x, y;
+    int x, y;
 
     // see if the board has an empty space and set the x and y
     int empty = 0;
@@ -33,12 +18,14 @@ int solve_board() {
                 break;
             }
         }
-        if (empty == 1) break;
+        if (empty == 1)
+            break;
     }
-    if (empty == 0) return 1;
+    if (empty == 0)
+        return 1;
 
     // track numbers already used in row and cols
-    int numbers[SIZE + 1] = { 0 };
+    int numbers[SIZE + 1] = {0};
     for (int i = 0; i < SIZE; i++) {
         numbers[board[x][i]] = 1;
         numbers[board[i][y]] = 1;
@@ -66,19 +53,22 @@ int solve_board() {
     return 0;
 }
 
-void read_board(char *path) {
-    FILE *file = fopen(path, "r");
+void read_board(const char* path) {
+    FILE* file = fopen(path, "r");
     char c;
     int i = 0, j = 0;
     while ((c = fgetc(file)) != EOF) {
-        if (c == ',') continue;
+        if (c == ',')
+            continue;
         if (c == '\n') {
             i += 1;
             j = 0;
             continue;
         }
-        if (c == 'x') board[i][j++] = EMPTY;
-        else board[i][j++] = c - '0';
+        if (c == 'x')
+            board[i][j++] = EMPTY;
+        else
+            board[i][j++] = c - '0';
     }
     fclose(file);
 }
@@ -87,7 +77,8 @@ void print_board() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             printf("%d", board[i][j]);
-            if (j < SIZE - 1) printf(",");
+            if (j < SIZE - 1)
+                printf(",");
         }
         printf("\n");
     }
@@ -96,4 +87,13 @@ void print_board() {
 int usage() {
     printf("usage: ./susol <input file>\n");
     return 1;
+}
+
+int main(int argc, const char** argv) {
+    if (argc != 2)
+        return usage();
+    read_board(argv[1]);
+    solve_board();
+    print_board();
+    return 0;
 }
